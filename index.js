3f25c8aa5acfd22785c9eb897bed3eefcd1234f5
Kaliholt <angel_cg_22_@hotmail.com>
2022-08-28 22:22:41 -0500

    Fusión del menú desplegable del Carrito de Compras

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
 const navMenuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const navShoppingIcon = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 navMenuIcon.addEventListener('click', toggleMobileMenu);
+navShoppingIcon.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu() {
+    productDetail.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    productDetail.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive')
+    productDetail.classList.toggle('inactive');
 }
